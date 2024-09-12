a5fc376dfe643a273e48ccb838977e035173e14e
KermRayan. <kermd237@gmail.com>
2024-09-13 00:24:04 +0100

    Update config.js

--- config.js
+++ config.js
@@ -10,24 +10,24 @@ module.exports = {
    ANTILINK: toBool(process.env.ANTI_LINK) || false,
    HEROKU: toBool(process.env.HEROKU) || false,
    AUTO_READ: toBool(process.env.AUTO_READ) || false,
-   AUTO_STATUS_READ: toBool(process.env.AUTO_STATUS_READ) || false,
+   AUTO_STATUS_READ: toBool(process.env.AUTO_STATUS_READ) || true,
    DELETED_LOG: toBool(process.env.DELETED_LOG) || true,
-   STATUS_SAVER: toBool(process.env.STATUS_SAVER) || true,
+   STATUS_SAVER: toBool(process.env.STATUS_SAVER) || false,
    AUTO_REACT: toBool(process.env.AUTO_REACT) || false,
 
    // String Values
-   SESSION_ID: process.env.SESSION_ID || "",
+   SESSION_ID: process.env.SESSION_ID || "", // Put your session
    HANDLERS: process.env.HANDLER === "false" || process.env.HANDLER === "null" ? "^" : "[.]",
-   BOT_INFO: process.env.BOT_INFO || "ᴀsᴛʀᴏ;ғxᴏᴘ-ᴍᴅ", // YourName;BotName;Image/VideoLink
-   SUDO: process.env.SUDO || "91234567899",
-   PRESENCE: process.env.PRESENCE || "unavailable", // "unavailable", "available", "composing", "recording", "paused"
-   CAPTION: process.env.CAPTION || "© ғxᴏᴘ ᴍᴅ",
+   BOT_INFO: process.env.BOT_INFO || "Kɢᴛᴇᴄʜ;KᴇʀᴍLɪᴛᴇ⚡️", // YourName;BotName;Image/VideoLink
+   SUDO: process.env.SUDO || "237656520674",
+   PRESENCE: process.env.PRESENCE || "available", // "unavailable", "available", "composing", "recording", "paused"
+   CAPTION: process.env.CAPTION || "© Kᴇʀᴍ ᴍᴅ",
    TIME_ZONE: process.env.TIME_ZONE || "Africa/Lagos",
    ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
    BRANCH: "master",
-   AUTHOR: process.env.AUTHOR || "ғxᴏᴘ-ᴍᴅ",
+   AUTHOR: process.env.AUTHOR || "Kᴇʀᴍ-ᴍᴅ",
    ANTIWORD: process.env.ANTIWORD || "badword1,badword2,badword3",
-   PACKNAME: process.env.PACKNAME || "ғxᴏᴘ-ᴍᴅ",
+   PACKNAME: process.env.PACKNAME || "KᴇʀᴍLɪᴛᴇ⚡️",
    WELCOME_MSG: process.env.WELCOME_MSG || "Hi @user Welcome to @gname",
    GOODBYE_MSG: process.env.GOODBYE_MSG || "Hi @user It was Nice Seeing you",
    RMBG_KEY: process.env.RMBG_KEY || "",
