9124c6953b77c62789f591a12660377009e15d38
KermRayan. <kermd237@gmail.com>
2024-09-13 11:38:44 +0100

    Update search.js

--- plugins/search.js
+++ plugins/search.js
@@ -40,7 +40,7 @@ Module(
       if (!data) return await message.send(`_${match} not found_`);
       const { name, timezone, sys, main, weather, visibility, wind } = data;
       const degree = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"][getFloor(wind.deg / 22.5 + 0.5) % 16];
-      return await message.send(`*Name :* ${name}\n*Country :* ${sys.country}\n*Weather :* ${weather[0].description}\n*Temp :* ${getFloor(main.temp)}°\n*Feels Like :* ${getFloor(main.feels_like)}°\n*Humidity :* ${main.humidity}%\n*Visibility  :* ${visibility}m\n*Wind* : ${wind.speed}m/s ${degree}\n*Sunrise :* ${moment.utc(sys.sunrise, "X").add(timezone, "seconds").format("hh:mm a")}\n*Sunset :* ${moment.utc(sys.sunset, "X").add(timezone, "seconds").format("hh:mm a")}`);
+      return await message.send(`*📇Name :* ${name}\n*🚩Country :* ${sys.country}\n*🌦️Weather :* ${weather[0].description}\n*❄️Temp :* ${getFloor(main.temp)}°\n*🌡️Feels Like :* ${getFloor(main.feels_like)}°\n*🥶Humidity :* ${main.humidity}%\n*🌬️Visibility  :* ${visibility}m\n*☀️Wind* : ${wind.speed}m/s ${degree}\n*🌄Sunrise :* ${moment.utc(sys.sunrise, "X").add(timezone, "seconds").format("hh:mm a")}\n*🌅Sunset :* ${moment.utc(sys.sunset, "X").add(timezone, "seconds").format("hh:mm a")}`);
    }
 );
 
