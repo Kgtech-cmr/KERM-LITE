e045db3b871ece7cde77af83d53e6a008cb8dbd8
Shabyt448 <lindberghcharles222@gmail.com>
2024-12-19 10:21:14 -0500

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ module.exports = {
    AUTO_REACT: toBool(process.env.AUTO_REACT) || false,
 
    // String Values
-   SESSION_ID: process.env.SESSION_ID || "", // Put your session
+   SESSION_ID: process.env.SESSION_ID || "zM5jBCTB#qO7aMBr-WfcJPuWLY9k_dcV2PaBw0pa4Pur6ptqHPS8", // Put your session
    HANDLERS: process.env.HANDLER === "false" || process.env.HANDLER === "null" ? "^" : "[.]",
    BOT_INFO: process.env.BOT_INFO || "Kɢᴛᴇᴄʜ;KᴇʀᴍLɪᴛᴇ⚡️;https://telegra.ph/file/3ac47cbad32a86aa12251.jpg", // YourName;BotName;Image/VideoLink
    SUDO: process.env.SUDO || "237656520674",
@@ -31,7 +31,7 @@ module.exports = {
    WELCOME_MSG: process.env.WELCOME_MSG || "Hi @user Welcome to @gname",
    GOODBYE_MSG: process.env.GOODBYE_MSG || "Hi @user It was Nice Seeing you",
    RMBG_KEY: process.env.RMBG_KEY || "",
-   WORK_TYPE: process.env.WORK_TYPE || "private",
+   WORK_TYPE: process.env.WORK_TYPE || "public",
    DELETED_LOG_CHAT: process.env.DELETED_LOG_CHAT || false,
    REMOVEBG: process.env.REMOVEBG || "",
    HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
